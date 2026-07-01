585bddc1316328f747c964c1ddf788a18968c454
Juan orellano <orellanojuansebastian@gmail.com>
2026-06-30 23:38:55 -0300

    Funcionalidades:cambios minimo en app y tarea, ahora app no tiene logica y solo muestra lo que se le ordena, en tarea ya tiene la logica para consumir la api pero le hace falta arreglos para que ande bien ya que esta no logra pasar los listados de las tareas, junto a eso en el proximo avance se debera mejorar el fomulario para ser compatible con la api, arrglos futuros:hacer un estilo mas agradable al nav, estudiar la logica junto a las funcione. atte:Juan orellano

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react"
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { Router, Route, Switch } from 'wouter'
-import Axios from "axios"
 import Header from "./componentes/Header";
 import ListadoDeTareas from "./componentes/Tarea"
 import Formulario from "./componentes/Formulario"
@@ -8,33 +8,34 @@ import "./App.css"
 
 
 export default function App() {
-    
 
     return (
         <div className="App">
+
             <Header />
-            <h1>Listado De Tareas</h1>
-            <div className="Contenedor"></div>
+
             <Router>
                 <Switch>
+
                     <Route path="/nueva">
                         <Formulario />
                     </Route>
+
                     <Route path="/listado">
-                        <ListadoDeTareas/>
-                        <h1>listado</h1>
+                         <ListadoDeTareas />
                     </Route>
+
                     <Route path="/">
-                        <h1>Componente APP</h1>
+                        <h1>Inicio</h1>
                     </Route>
+
                     <Route>
                         <h1>Pagina no encontrada</h1>
                     </Route>
+
                 </Switch>
             </Router>
 
-
         </div>
-
-    )
+    );
 }
--- src/componentes/Tarea.jsx
+++ src/componentes/Tarea.jsx
@@ -1,21 +1,84 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Tarjeta from "./Tarjeta";
 
-export default function ListadoDeTareas({ tareas, eliminar }) {
+export default function Tarea() {
+
+    const [tareas, setTareas] = useState([]);
+
+    const actualizar = () => {
+
+        const url = "https://api-tareas.ctpoba.edu.ar/api/tareas";
+
+        const config = {
+            headers: {
+                Authorization: '48191403'
+            }
+        };
+
+        axios.get(url, config)
+            .then((resp) => {
+                console.log(resp.data);
+                setTareas(resp.data.tareas);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+
+    };
+
+    useEffect(() => {
+        actualizar();
+    }, []);
+
+    const eliminar = (tarea_id) => {
+
+        const url = `https://api-tareas.ctpoba.edu.ar/api/tareas/${tarea_id}`;
+
+        const config = {
+            headers: {
+                Authorization: '48191403'
+            }
+        };
+
+        axios.delete(url, config)
+            .then(() => {
+                alert("Tarea eliminada");
+            })
+            .catch((error) => {
+                console.error(error);
+            })
+            .finally(() => {
+                actualizar();
+            });
+
+    };
+
+    const prioridades = {
+        1: "urgente",
+        2: "importante",
+        3: "leve"
+    };
 
     return (
         <div style={{ flex: 3 }}>
             <div className="ListadoDeTareas">
-                {tareas.map((tarea) =>
+
+                {tareas && tareas.map((tarea) => (
+
                     <Tarjeta
                         key={tarea.id}
-                        prioridad={tarea.prioridad}
-                        titulo={tarea.titulo}
-                        categoria={tarea.categoria}
+                        titulo={tarea.nombre}
                         descripcion={tarea.descripcion}
+                        categoria={tarea.categoria}
+                        prioridad={prioridades[tarea.prioridad]}
+                        estado={tarea.estado}
                         eliminar={() => eliminar(tarea.id)}
                     />
-                )}
+
+                ))}
+
             </div>
         </div>
-    )
+    );
 }
